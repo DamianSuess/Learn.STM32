b4452c459b1c868b8ad5e4ceb4c85cf229d58036
Damian Suess <djsuess@gmail.com>
2026-03-14 12:15:07 -0400

    Added Git Submodule Unity unit-testing framework. Updated vscode template files. EditorConfig now ignores 3rd-party "/libs/**" formatting rules from overriding our rules

---
+++ source/Test-STM32H5-UnityUnitTest/readme.md
@@ -0,0 +1,13 @@
+# STM32 with Unity Unit-Testing Framework
+
+The Unity unit-testing framework _"is written in 100% pure ANSI C._" and is intended for tiny 8-bit MCU to 64-bit ones as well.
+
+## Jump-Start Usage
+
+## Configuring
+
+1. Add folders to your project
+   * `tests`
+   * `unity`
+2. Upload [Unity](https://github.com/ThrowTheSwitch/Unity/releases) into the `unity` folder
+   * (ideally use, gitmodule, or some other form)
---
+++ source/Test-STM32H5-UnityUnitTest/tests/test_example.c
@@ -0,0 +1,42 @@
+// Copyright 2026 Suess Labs, Inc. All rights reserved.
+
+#include "unity.h"
+#include "unity_internals.h"
+// #include "test_example.h"
+
+#define UNITY_INCLUDE_PRINT_FORMATTED
+
+void test_example(void);
+
+int main()
+{
+  UNITY_BEGIN();
+  RUN_TEST(test_example);
+
+  ////// protect against infinite loops or long-running tests
+  ////if (TEST_PROTECT())
+  ////{
+  ////  // Additional test cases can be added here
+  ////  test_example();
+  ////}
+
+  return UNITY_END();
+}
+
+void setUp(void)
+{
+  // Setup before EACH TEST
+}
+
+void tearDown()
+{
+  // Cleanup after each test
+}
+
+void test_example(void)
+{
+  // Example test case
+  TEST_ASSERT_EQUAL(1, 1);
+
+  // TEST_PRINTF("This is an example test case.\n");
+}
